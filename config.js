dc78e3e7867b8e48e829a01ce15604ba2c467898
yonasadiel <yonazadielwiguna@gmail.com>
2018-02-17 13:25:39 +0700

    initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+var channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN;
+var channelSecret      = process.env.CHANNEL_SECRET;
+var commandSymbol      = process.env.COMMAND_SYMBOL;
+
+module.exports = {
+  channelAccessToken : channelAccessToken,
+  channelSecret      : channelSecret,
+  commandSymbol      : commandSymbol
+};
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const line    = require('@line/bot-sdk')
+const fs      = require('fs')
+const config  = require('./config')
+
+const data_folder    = 'data'
+const command_folder = 'command'
+
+const app = express()
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+})
+
+const client = new line.Client(config)
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null)
+  }
+
+  if (event.message.text.startsWith(config.commandSymbol)) {
+    args = []
+    args_i = 0
+    quote = null
+    text = event.message.text
+    for (var i=0; i<text.length; i++) {
+      if (text[i] === quote) { quote = '' }
+      else if (quote !== null && (text[i] === '\'' || text[i] === '"')) {
+        quote = text[i]
+      } else if (text[i] === ' ') {
+        args_i++
+        args.push('')
+      } else {
+        args[args_i] += text[i]
+      }
+    }
+
+    console.log(args)
+    fs.readdir(command_folder, (err, files) => {
+      if (err) {
+        client.replyMessage(event.replyToken, {
+          type: 'text',
+          text: err
+        })
+      } else {
+        for (var i=0; i<files.length; i++) {
+          var found = false
+          if (config.commandSymbol + files[i] === args[0] + ".js") {
+            const command = require(command_folder + files[i])
+            found = true
+            command.receive(args, client, event)
+          }
+          if (!found) {
+            client.replyMessage(event.replyToken, {
+              type: 'text',
+              text: 'no command found'
+            })
+          }
+        }
+      }
+    });
+
+  }
+}
+
+app.listen(process.env.PORT || 5000)
