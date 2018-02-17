5717d753017ca6e4b52a8cff87a7511b0822f9d6
yonasadiel <yonazadielwiguna@gmail.com>
2018-02-17 16:19:34 +0700

    add remind

---
+++ command/remind.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+
+const data_folder = '/app/data/'
+
+function sortByDue(a, b) {
+  if (a.due < b.due) return -1;
+  if (a.due > b.due) return 1;
+  return 0;
+}
+
+module.exports = {
+  receive  : function(args, client, event) {
+
+    fs.readdir(data_folder, (err, files) => {
+      if (err) {
+        console.log(err)
+      } else {
+        for (var i=0; i<files.length; i++) {
+          var id = files[i].substring(0, files[i].length-3)
+          var filename = data_folder + files[i]
+          var remindText = ''
+          fs.readFile(filename, 'utf8', (err, data) => {
+            var obj = []
+            if (err) {
+              console.log(err)
+            } else {
+              obj = JSON.parse(data)
+              obj.sort(sortByDue)
+              for (var i=0; i<obj.length; i++) {
+                remindText += (i+1) + '. ' + obj[i].due + ' ' + obj[i].desc
+                if (i !== obj.length -1) { remindText += '\n' }
+              }
+              client.pushMessage(id, {
+                type: 'text',
+                text: remindText
+              })
+            }
+          })
+        }
+      }
+    });
+
+    
+  }
+};
